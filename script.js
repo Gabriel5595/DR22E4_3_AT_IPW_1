15a8a6c5b1ebdf002f0ed9b30c0e43832efb7305
Gabriel Carvalho <110797653+Gabriel5595@users.noreply.github.com>
2022-12-16 23:49:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const nome = prompt("Digite o seu nome");
+const idade = prompt("Digite a sua idade");
+const email = prompt("Digite o seu email");
+const sexo = prompt("Digite o seu sexo");
+
+const pessoa = {
+  nome,
+  idade,
+  email,
+  sexo,
+}
+
+console.log(pessoa);
